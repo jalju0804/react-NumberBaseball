789999d2819d8aaee986238fac19f8346388c7e7
jalju0804 <dlckswn334@naver.com>
2022-09-04 14:18:15 +0900

    Hooks 적용

--- BBgame.jsx
+++ BBgame.jsx
@@ -1,4 +1,4 @@
-import React,{Component} from 'react';
+import React,{ useState } from 'react';
 import Try from './Try';
 
 function getNumbers(){
@@ -11,80 +11,68 @@ function getNumbers(){
     return array;
 }
 
-class BBgame extends Component {
-    state = {
-        result : '',
-        value : '',
-        answer: getNumbers(),
-        tries : [], // push 쓰면 안됨 기존 array에서 변경이 일어나서 react에서 감지를 못함 즉 참조가 바뀌어야함
-    };
+const BBgame = () =>{
+    const [result,setResult] = useState('');
+    const [value,setValue] = useState('');
+    const [answer,setAnswer] = useState(getNumbers); //lazy init useState는 값을 넣는 경우 함수를 넣는 경우가 있는데 함수를 넣으면 처음 실행될 때 함수의 return이 들어가고 다시 render 할 떄는 실행되지 않음
+    const [tries,setTries] = useState([]);
 
-    onSubmitForm = (e) => { 
+    const onSubmitForm = (e) => { 
         e.preventDefault();
-        if(this.state.value === this.state.answer.join('')){
-            this.setState({
-                result : '홈런!',
-                tries: [...this.state.tries,{try : this.state.value, result : '홈런!'}]
-            })
-            alert('게임을 다시 시작합니다!');
-            this.setState({
-                value: '',
-                answer: getNumbers(),
-                tries : [],
+        if(value === answer.join('')){
+            setResult('홈런!');
+            setTries((prevTries)=>{
+                return [...prevTries,{try : value, result : '홈런!'}]
             });
+            alert('홈런! 게임을 다시 시작합니다!');
+            setValue('');
+            setAnswer(getNumbers());
+            setTries([]);
         } else {
-            const answerArray = this.state.value.split('').map((v) => parseInt(v));
+            const answerArray = value.split('').map((v) => parseInt(v));
             let strike = 0;
             let ball = 0;
-            if(this.state.length >= 9){
-                this.setState({
-                    result : `10번 넘게 틀려서 실패! 답은 ${this.state.answer.join(',')}였습니다`,
-                });
+            if(length >= 9){
+                setResult(`10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다`);
                 alert('게임을 다시 시작합니다!');
-                this.setState({
-                    value: '',
-                    answer: getNumbers(),
-                    tries : [],
-                });
+                setValue('');
+                setAnswer(getNumbers()); // getNumbers를 넣어도 함수를 넣은 판정이 돼서 돌아가나 getNumbers()를 넣어 호출하는게 바람직함
+                setTries([]);
             }else{
                 for(let i = 0;i<4;i+=1){
-                    if(answerArray[i] === this.state.answer[i]){
+                    if(answerArray[i] === answer[i]){
                         strike += 1;
-                    } else if(this.state.answer.includes(answerArray[i])){
+                    } else if(answer.includes(answerArray[i])){
                         ball += 1;
                     }
                 }
-                this.setState({
-                        tries: [...this.state.tries, {try : this.state.value, result : `${strike} 스트라이크, ${ball} 볼 입니다.` }]
+                setTries((prevTries)=>{
+                    return [...prevTries, {try : value, result : `${strike} 스트라이크, ${ball} 볼 입니다.` }]
                 })
             }
         }
     };
 
-    onChangeInput = (e) => {
-        this.setState({
-           value : e.target.value,
-        });
+    const onChangeInput = (e) => {
+        setValue(e.target.value);
     }
 
-    render(){
-        return(
-            <>
-                <h1>{this.state.result}</h1>
-                <form onSubmit={this.onSubmitForm}>
-                    <input maxLength={4} value = {this.state.value} onChange = {this.onChangeInput}></input>
-                </form>
-                <div>시도: {this.state.tries.length}</div>
-                <ul>
-                    {this.state.tries.map((v,i) => {
-                        return (
-                           <Try key={`${i+1}차 시도 : `} tryInfo={v}/>
-                        );
-                    })}  
-                </ul>
-            </>
-        );
-    }
-}
+    return(
+       <div>
+        <h1>{result}</h1>
+        <form onSubmit={onSubmitForm}>
+            <input maxLength={4} value = {value} onChange = {onChangeInput}></input>
+        </form>
+        <div>시도: {tries.length}</div>
+        <ul>
+            {tries.map((v,i) => {
+                return (
+                   <Try key={`${i+1}차 시도 : `} tryInfo={v}/>
+                );
+            })}  
+        </ul>
+        </div>
+    );
+};
 
 export default BBgame;
--- client.jsx
+++ client.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDom from 'react-dom/client';
 
 import BBgame from './BBgame';
 
-ReactDom.render(<BBgame />, document.querySelector('#root'));
-//ReactDom.createRoot(document.querySelector('#root')).render(<BBgame/>);
+ReactDom.createRoot(document.querySelector('#root')).render(<BBgame/>);
